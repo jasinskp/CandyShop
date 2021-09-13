b5fb2674787b19d85c84d7740512663cf5f6c1f2
Powell <pawel.jasinski.82@gmail.com>
2021-09-13 15:24:02 +0200

    Added Casandra DB

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jasinskp/CandyShop/pkg/http/rest"
+)
+
+func main() {
+	fmt.Println("Server starting on port 8080")
+
+	router := rest.InintHandlers()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
