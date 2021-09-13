528b18200a30373d611be1b71982c6babfdc7d33
Powell <pawel.jasinski.82@gmail.com>
2021-09-13 14:04:13 +0200

    Inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jasinskp/CandyShop
+
+go 1.17
---
+++ pkg/http/rest/handler.go
@@ -0,0 +1,9 @@
+package handler
+
+import "github.com/gorilla/mux"
+
+func InintHandlers() *mux.Router {
+	router := mux.NewRouter()
+
+	return router
+}
